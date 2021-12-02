09a5a1dc379515f289b09feebbb0ece5bfb320f6
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-12-02 14:19:08 +0530

    Linked list problemo

---
+++ LinkedListPrograms/LinkedListPrograms/ListFunctions.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListPrograms
+{
+    public class ListFunctions
+    {
+        public Node head;
+        public void Add(int number)
+        {
+            Node newNode = new Node(number);
+            if(head==null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next!=null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = newNode;
+
+                
+            }
+            Console.WriteLine(number + " Data Entered");
+        }
+       
+        public void deleteFirst()
+        {
+            if(this.head==null)
+            {
+                Console.WriteLine("Nothing to delete");
+            }
+            else
+            {
+                Node temp=head;
+                head = head.next;
+                temp.next = null;
+                Console.WriteLine("Value  delete from first");
+            }
+
+        }
+        public void deleteLast()
+        {
+            if(head==null)
+            {
+                Console.WriteLine("Nothing to delete");
+            }
+            else
+            { 
+                Node temp = head;
+                while(temp.next.next!=null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = null;
+                Console.WriteLine("Value Deleted from last");
+            }
+        }
+        public void delete(int number)
+        {
+            Node temp = head;
+            while(temp.next!=null && temp.next.data!=number)
+            {
+                    temp = temp.next;
+            }
+            Node nxt = temp.next;
+            temp.next = nxt.next;
+            nxt.next = null;
+
+
+        }
+        public bool search(int number)
+        {
+            Node temp = head;
+            while(temp.next!=null)
+            {
+                if(temp.data==number)
+                {
+                    return true;
+                }
+                temp = temp.next;
+            }
+
+
+            return false;
+
+        }
+        
+        public void display()
+        {
+            if(head==null)
+            {
+                Console.WriteLine("Nothing to diaplsy");
+            }
+            else
+            {
+                Node temp = head;
+                while(temp!=null)
+                {
+                    Console.Write(" " + temp.data);
+                    temp = temp.next;
+                }
+            }
+            Console.WriteLine();
+        }
+
+
+    }
+}
---
+++ LinkedListPrograms/LinkedListPrograms/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LinkedListPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListFunctions obj = new ListFunctions();
+            obj.Add(56);
+            obj.Add(30);
+            obj.Add(70);
+           
+
+            obj.display();
+
+            Console.WriteLine("------delete first---------");
+
+            obj.deleteFirst();
+            obj.display();
+            Console.WriteLine("-------delete last--------");
+
+            obj.deleteLast();
+            obj.display();
+            Console.WriteLine("------delete---------");
+            obj.Add(10);
+            obj.Add(90);
+            obj.Add(25);
+            obj.Add(999);
+
+            obj.delete(90);
+            obj.display();
+
+        }
+    }
+}
